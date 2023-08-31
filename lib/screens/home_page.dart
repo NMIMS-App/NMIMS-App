252072c49a39f342ea3b08f2696dd8ec0a35e45f
vedantheda <vedant.heda@outlook.com>
2023-08-31 17:35:42 +0530

    Home Page completed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nmims_app/widgets/category_item.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -13,58 +14,256 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            IconButton(
+              icon: const Icon(
+                Icons.person,
+                color: Colors.black,
+                size: 30,
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Stack(
-                  clipBehavior: Clip.hardEdge,
+              onPressed: () {
+                // Add your assignments icon tap logic here
+              },
+            ),
+            const SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'John Doe',
+                  style: GoogleFonts.inter(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const Text.rich(
+                  TextSpan(
+                    text: 'B.Tech Computer - ',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'SEM V',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            IconButton(
+              icon: const Icon(
+                Icons.assignment,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {
+                // Add your attendance icon tap logic here
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.touch_app,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {
+                // Add your attendance icon tap logic here
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.notifications,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {
+                // Add your announcements icon tap logic here
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                ),
+                child: Column(
                   children: [
-                    Container(
-                      height: 80,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xFFFF5F5F),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Stack(
+                        clipBehavior: Clip.hardEdge,
+                        children: [
+                          Container(
+                            height: 80,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: const Color(0xFFFF5F5F),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Attendance',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    overflow: TextOverflow.visible,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: -20,
+                            top: -20,
+                            child: Container(
+                              height: 120,
+                              width: 120,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xFF58585A),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '80',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Positioned(
-                      right: -20,
-                      top: -20,
-                      child: Container(
-                        height: 120,
-                        width: 120,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFF58585A),
-                        ),
-                        child: const Positioned(
-                          child: Text(
-                            '3',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Stack(
+                        clipBehavior: Clip.hardEdge,
+                        children: [
+                          Container(
+                            height: 80,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: const Color(0xFFFF5F5F),
                             ),
-                            textAlign: TextAlign.center,
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Pending \nAssignments',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    overflow: TextOverflow.visible,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
+                          Positioned(
+                            right: -20,
+                            top: -20,
+                            child: Container(
+                              height: 120,
+                              width: 120,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xFF58585A),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '3',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
+                    ),
+                    const SizedBox(
+                      height: 30,
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: const Color(0xFFFF6161),
+              ),
+              height: 800,
+              width: double.infinity,
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                padding: const EdgeInsets.all(25),
+                children: const [
+                  CategoryItem(
+                    categoryName: 'Attendance',
+                    categoryIcon: Icons.touch_app,
+                  ),
+                  CategoryItem(
+                    categoryName: 'Assignments',
+                    categoryIcon: Icons.assignment,
+                  ),
+                  CategoryItem(
+                    categoryName: 'Announcements',
+                    categoryIcon: Icons.notifications,
+                  ),
+                  CategoryItem(
+                    categoryName: 'Time Table',
+                    categoryIcon: Icons.table_chart,
+                  ),
+                  CategoryItem(
+                    categoryName: 'Syllabus',
+                    categoryIcon: Icons.book,
+                  ),
+                  CategoryItem(
+                    categoryName: 'Results',
+                    categoryIcon: Icons.assessment,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            )
+          ],
         ),
       ),
     );
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, this.categoryName, this.categoryIcon});
+
+  final String? categoryName;
+  final IconData? categoryIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              categoryIcon,
+              size: 60,
+              color: Colors.black,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              categoryName!,
+              style: GoogleFonts.inter(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
