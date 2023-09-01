cab2669c2d85923079d098edf2e9e1d91603d9ef
vedantheda <vedant.heda@outlook.com>
2023-09-01 13:15:37 +0530

    Inkwell

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -9,32 +9,39 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              categoryIcon,
-              size: 60,
-              color: Colors.black,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              categoryName!,
-              style: GoogleFonts.inter(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+    return InkWell(
+      borderRadius: BorderRadius.circular(40), // same as the container's radius
+      onTap: () {
+        // TODO: Handle tap action here.
+        print('$categoryName tapped!');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                categoryIcon,
+                size: 60,
                 color: Colors.black,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                categoryName!,
+                style: GoogleFonts.inter(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
