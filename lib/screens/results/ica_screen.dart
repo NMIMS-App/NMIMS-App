e9c497e97550ccb141092bdda9976ca28e711a59
Nikita Melam <nikita.melam@gmail.com>
2023-09-13 18:47:27 +0530

    ICA item model elements implemented in ICAScreen

--- lib/screens/results/ica_screen.dart
+++ lib/screens/results/ica_screen.dart
@@ -3,16 +3,16 @@ import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nmims_app/models/ica_item_model.dart';
 
 class ICAScreen extends StatefulWidget {
   @override
-State<StatefulWidget> createState() => ICAPageState();
+  State<StatefulWidget> createState() => ICAPageState();
 
 }
 
 class ICAPageState extends State<ICAScreen> {
-  var details =[["Mobile Application Development", 45, "PASS", "true"],["Calculus", 21, "FAIL", "true"], ["Operating Systems", 39, "PASS", "true"], ["Software Engineering", 42, "PASS", "true"], ["Quantum and Statistical Physics", 12, "FAIL", "true"], ["Computer Networks",30, "PASS", "true"]];
-  var marks = [[2023, "Semester 5", 10, 9, 26] , [2022, "Semester 3", 7, 5, 9], [2022, "Semester 4", 8, 9, 22],[2021, "Semester 2", 9, 9, 24], [2021, "Semester 2", 2, 3, 7], [2023, "Semester 5", 7, 9, 14]];
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -26,7 +26,6 @@ class ICAPageState extends State<ICAScreen> {
         ),
 
         body:Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: const EdgeInsets.all(10.0),
@@ -75,17 +74,8 @@ class ICAPageState extends State<ICAScreen> {
             Expanded(child:
             ListView.builder(itemBuilder: (context, index){
 
-              var detMarks = details[index][1];
-              var sem = marks[index][1];
-              var yr = marks[index][0];
-              var m1 = marks[index][2];
-              var m2 = marks[index][3];
-              var tw = marks[index][4];
-
-              bool vis = details[index][3].toString().toLowerCase() != "false";
-
-              Color textColor = details[index][2]=="PASS" ? Colors.green : Colors.red;
-              Color boxColor = details[index][2]=="PASS" ? CupertinoColors.systemGrey6: Color.fromRGBO(250, 227, 229, 1);
+              Color textColor = ica[index].icaStatus=="PASS" ? Colors.green : Colors.red;
+              Color boxColor = ica[index].icaStatus=="PASS" ? CupertinoColors.systemGrey6: Color.fromRGBO(250, 227, 229, 1);
               //  Color.fromRGBO(230, 252, 234, 1) for Green 232, 235, 234
               return SingleChildScrollView(
 
@@ -95,10 +85,8 @@ class ICAPageState extends State<ICAScreen> {
                   child: Column(
                     children: [
                       Visibility(
-                        // visible: vis,
-                        // if(details[index][3])
-                        // visible: bool.fromEnvironment(details[index][3].toString()),
-                        visible: details[index][3].toString()== "true" ? true:false,
+
+                        visible: ica[index].icaVisibility,
 
                         child: Container(
                           margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
@@ -114,34 +102,32 @@ class ICAPageState extends State<ICAScreen> {
                                     shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
                                 ),
                                 onPressed: (){
-                                  if(details[index][3].toString()=="true"){
+                                  if(ica[index].icaVisibility==true){
 
-                                    details[index][3]="false";
-                                    print(details[index][3]);
+                                    ica[index].icaVisibility=false;
                                     setState(() {
 
                                     });
                                   }
-                                  else if(details[index][3].toString()=="false"){
+                                  else if(ica[index].icaVisibility==false){
 
-                                    details[index][3]="true";
-                                    print(details[index][3]);
+                                    ica[index].icaVisibility=true;
                                     setState(() {
 
                                     });
                                   }
 
-                                  print(details[index][3]);
+                                  // print(details[index][3]);
                                 },
                                 child: Column(
                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                   children: [
-                                    Text(details[index][0].toString(), style: TextStyle(fontSize: 20, color: Colors.red),),
+                                    Text(ica[index].icaCourse.toString(), style: TextStyle(fontSize: 20, color: Colors.red),),
                                     Row(
                                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                       children: [
-                                        Text("Marks: $detMarks", style: TextStyle(fontSize: 18, color: Colors.black, ),),
-                                        Text(details[index][2].toString(), style: TextStyle(color: textColor, fontSize: 18, fontWeight: FontWeight.w900),)
+                                        Text("Marks: ${ica[index].icaTotal}", style: TextStyle(fontSize: 18, color: Colors.black, ),),
+                                        Text(ica[index].icaStatus, style: TextStyle(color: textColor, fontSize: 18, fontWeight: FontWeight.w900),)
                                       ],
                                     )
                                   ],
@@ -151,14 +137,16 @@ class ICAPageState extends State<ICAScreen> {
                         ),
                       ),
                       Visibility(
-                          visible: details[index][3].toString()=="false"?true:false,
+
+                          // visible: details[index][3].toString()=="false"?true:false,
+                        visible: !ica[index].icaVisibility,
                           child: Container(
 
                             margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
                             child: OutlinedButton(
 
                               onPressed: (){
-                                details[index][3]="true";
+                                ica[index].icaVisibility=true;
                                 setState(() {
 
                                 });
@@ -175,33 +163,33 @@ class ICAPageState extends State<ICAScreen> {
                                 children: [
                                   Padding(
                                     padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                    child: Text(details[index][0].toString(), style: TextStyle(fontSize: 20, color: Colors.red,fontWeight: FontWeight.w700, overflow: TextOverflow.fade),),
+                                    child: Text(ica[index].icaCourse.toString(), style: TextStyle(fontSize: 20, color: Colors.red,fontWeight: FontWeight.w700, overflow: TextOverflow.fade),),
                                   ),
-                                  Text("$sem", style: TextStyle(color: Colors.black, fontStyle: FontStyle.italic),),
-                                  Text("Year: $yr", style: TextStyle(color: Colors.black, fontStyle: FontStyle.italic),),
+                                  Text("Semester: ${ica[index].icaSemester}", style: TextStyle(color: Colors.black, fontStyle: FontStyle.italic),),
+                                  Text("Year: ${ica[index].icaYear}", style: TextStyle(color: Colors.black, fontStyle: FontStyle.italic),),
                                   Row(
                                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                                     children: [
                                       Padding(
                                         padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                                        child: Text("M-1 Marks: $m1/10", style: TextStyle(color: Colors.black, fontSize: 15),),
+                                        child: Text("M-1 Marks: ${ica[index].icaM1}/10", style: TextStyle(color: Colors.black, fontSize: 15),),
                                       ),
                                       Padding(
                                         padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                                        child: Text("M-2 Marks: $m2/10", style: TextStyle(color: Colors.black, fontSize: 15),),
+                                        child: Text("M-2 Marks: ${ica[index].icaM2}/10", style: TextStyle(color: Colors.black, fontSize: 15),),
                                       )
                                     ],
                                   ),
                                   Row(
                                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                     children: [
-                                      Text("Term Work: $tw/30", style: TextStyle(color: Colors.black,  fontSize: 15),),
-                                      Text("Total Marks: $detMarks/50", style: TextStyle(color: Colors.black,  fontSize: 15, fontWeight: FontWeight.w900),)
+                                      Text("Term Work: ${ica[index].icaTermWork}/30", style: TextStyle(color: Colors.black,  fontSize: 15),),
+                                      Text("Total Marks: ${ica[index].icaTotal}/50", style: TextStyle(color: Colors.black,  fontSize: 15, fontWeight: FontWeight.w900),)
                                     ],
                                   ),
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: Text(details[index][2].toString(), style: TextStyle(color: textColor, fontSize: 18, fontWeight: FontWeight.w900),),
+                                    child: Text(ica[index].icaStatus, style: TextStyle(color: textColor, fontSize: 18, fontWeight: FontWeight.w900),),
                                   )
                                 ],
                               ),
@@ -213,7 +201,7 @@ class ICAPageState extends State<ICAScreen> {
 
               );
 
-            }, itemCount: details.length,),
+            }, itemCount: ica.length,),
             ),
 
 
